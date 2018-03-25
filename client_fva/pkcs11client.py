163233e5301385bb09fd732b7e132aeedece8b30
Luis Zárate M <luis.zarate@solvosoft.com>
2018-03-24 18:39:46 -0600

    not working

--- client_fva/pkcs11client.py
+++ client_fva/pkcs11client.py
@@ -12,9 +12,31 @@ import OpenSSL
 import platform
 from client_fva import signals
 import logging
-
+import datetime
 logger = logging.getLogger('dfva_client')
 
+
+class Token:
+    serial=None
+    def __init__(serial):
+        self.serial = serial
+    
+
+class Slot:
+    def __init__(self, serial):
+        self.serial = serial
+    
+    def get_token(self):
+        return Token(self.serial)        
+
+class PrivateKey:
+    dirpath = None
+
+    def __init__(dirpath):
+        self.dirpath=dirpath 
+    def sign(self, data):
+        pass
+        
 
 class PKCS11Client:
     slot = None
@@ -24,209 +46,87 @@ class PKCS11Client:
     settings = None
     keys = None
     identification = None
-
+    session=None
     def __init__(self, *args, **kwargs):
-
         self.settings = kwargs.get('settings', {})
-        self.signal = kwargs.get('signal', None)
-        self.slot = kwargs.get('slot', self.get_slot())
+
 
     def get_slot(self):
         """Obtiene el primer slot (tarjeta) disponible
         .. warning:: Solo usar en pruebas y mejorar la forma como se capta
         """
-        if self.slot:
-            return self.slot
-        try:
-            lib = pkcs11.lib(self.get_module_lib())
-            slots = lib.get_slots()
-        except Exception as e:
-            self.signal.send('notify', obj={
-                'message': "La biblioteca instalada no funciona para leer las tarjetas, esto puede ser porque no ha instalado las bibliotecas necesarias o porque el sistema operativo no está soportado"
-            })
-            logger.error("Error abriendo dispositivos PKCS11 %r" % (e,))
-
-        if not slots:
-            raise Exception("PKCS11: Slot not found")
-        self.slot = slots[0]
-        return self.slot
+        return [Slot(b'446015340194853967119918706694137765024581677'), 
+                Slot(b'446015340196629408544548422120241811584796718')]
 
     def get_module_lib(self):
         """Obtiene la biblioteca de comunicación con la tarjeta """
+         return '/usr/lib/libASEP11.so'
 
-        if hasattr(self.settings, 'module_path') and self.settings.module_path:
-            return self.settings.module_path
-
-        if 'PKCS11_MODULE' in os.environ:
-            return os.environ['PKCS11_MODULE']
-
-        if os.path.exists('/usr/lib/libASEP11.so'):  # Linux
-            return '/usr/lib/libASEP11.so'
-
-        if os.path.exists("/usr/local/lib/libASEP11.dylib"):  # macOS
-            return "/usr/local/lib/libASEP11.dylib"
-
-        # FIXME: Hacer la construcción del path por defecto para windows,
-        # sugerencia
-        """
-        public static String ObtenerDirectorioDeWindows()
-          {
-            String direccionDeWindows = System.getenv("SystemRoot");
-            if ((direccionDeWindows == null) || (direccionDeWindows.equalsIgnoreCase(""))) {
-              direccionDeWindows = System.getenv("WINDIR");
-            }
-            String directorioDeWindows = direccionDeWindows + File.separator + "system32";
-            return directorioDeWindows;
-          }
-        """
-
-        _os = platform.system().lower()
-        _os_arch = platform.machine()
-        BASE_DIR = os.path.dirname(
-            os.path.dirname(os.path.abspath(__file__)))
-        if _os == 'linux':
-            path = os.path.join(
-                BASE_DIR, 'client_fva/libs/%s/%s/libASEP11.so' % (_os, _os_arch))
-        elif _os == "darwin":
-            path = os.path.join(
-                BASE_DIR, 'client_fva/libs/macos/libASEP11.dylib')
-        elif _os == "windows":
-            path = os.path.join(
-                BASE_DIR, 'client_fva/libs/windows/asepkcs.dll')
-
-        if os.path.exists(path):
-            return path
-
-        self.signal.send('notify', obj={
-            'message': "No existe una biblioteca instalada para leer las tarjetas, esto puede ser porque no ha instalado las bibliotecas necesarias o porque el sistema operativo no está soportado"
-        })
 
     def get_pin(self, pin=None):
         """Obtiene el pin de la tarjeta para iniciar sessión"""
-
-        if pin:
-            return pin
-
-        if 'PKCS11_PIN' in os.environ:
-            return os.environ['PKCS11_PIN']
-        else:
-            try:
-                serial = self.get_slot().get_tokens()[0].serial.decode('utf-8')
-            except:
-                serial = 'N/D'
-                # Fixme: aqui debería manejarse mejor
-            respobj = signals.get_signal_response(
-                self.signal.send('pin', obj={
-                    'serial': serial}))
-            return respobj.response['pin']
-
-        raise Exception(
-            'Sorry PIN is Needed, we will remove this, but for now use export PKCS11_PIN=<pin> before call python')
+        return "889888"
 
     def get_session(self, pin=None):
         """Obtiene o inicializa una sessión para el uso de la tarjeta.
         .. warning:: Ojo cachear la session y revisar si está activa
         """
-        # Fixme: Verificar si la sessión está activa y si no lo está entonces
-        # volver a iniciarla
-        if self.session is None:
-            self.token = self.slot.get_token()
-            self.session = self.token.open(user_pin=self.get_pin(pin=pin))
-            return self.session
-        return self.session
+        return None
+
+    def read_public_from_disc(dirpath):
+        x509 = OpenSSL.crypto.load_certificate(
+                    OpenSSL.crypto.FILETYPE_ASN1, open(dirpath).read())
+        return OpenSSL.crypto.dump_publickey(OpenSSL.crypto.FILETYPE_PEM, x509.get_pubkey())
+
+    def read_certificate_from_disc(dirpath)
+        x509 = OpenSSL.crypto.load_certificate(
+                    OpenSSL.crypto.FILETYPE_ASN1, open(dirpath).read())
+        return OpenSSL.crypto.dump_certificate(
+                    OpenSSL.crypto.FILETYPE_PEM, x509)
 
     def get_certificates(self):
         if self.certificates:
             return self.certificates
 
-        slot = self.get_slot()
-        certs = []
-        token = slot.get_token()
-        with token.open() as session:
-            for cert in session.get_objects({
-                    Attribute.CLASS: ObjectClass.CERTIFICATE}):
-                x509 = OpenSSL.crypto.load_certificate(
-                    OpenSSL.crypto.FILETYPE_ASN1, cert[Attribute.VALUE])
-                certs.append(OpenSSL.crypto.dump_certificate(
-                    OpenSSL.crypto.FILETYPE_PEM, x509))
-
-        # FIXME: not positional extraction
-        self.certificates = {
-            'authentication': certs[0],
-            'sign': certs[1]
-        }
+        self.certificates = {'sign':  self.read_certificate_from_disc('certs/sign.crt'), 
+                            'authentication': self.read_certificate_from_disc('certs/auth.crt') }
         return self.certificates
 
     def get_certificate_info(self):
-
-        info = []
-        slot = self.get_slot()
-        token = slot.get_token()
-        with token.open() as session:
-            for cert in session.get_objects({
-                    Attribute.CLASS: ObjectClass.CERTIFICATE}):
-                x509 = OpenSSL.crypto.load_certificate(
-                    OpenSSL.crypto.FILETYPE_ASN1, cert[Attribute.VALUE])
-                subject = x509.get_subject()
-                name = "%s %s" % (subject.GN, subject.SN)
-                identification = subject.serialNumber
-                person = {
-                    'name': name.title(),
-                    'identification': identification.replace("CPF-", ''),
-                    'type': subject.O,
-                    'organization': subject.OU,
-                    'country': subject.C,
-                    'commonName': subject.commonName,
-                    'serialNumber': subject.serialNumber,
-                    'cert_serialnumber': x509.get_serial_number(),
-                    'cert_start': parse(x509.get_notBefore()),
-                    'cert_expire': parse(x509.get_notAfter())
-                }
-                info.append(person)
-
-        return info
+         return [ {'identification': '03-0449-0054', 
+                   'type': 'PERSONA FISICA', 
+                   'organization': 'CIUDADANO', 
+                   'serialNumber': 'CPF-03-0449-0054', 
+                   'cert_start': datetime.datetime(2017, 3, 17, 14, 8, 58),
+                   'name': 'Marta Eugenia Solano Gutierrez', 
+                   'cert_serialnumber': 446015340194853967119918706694137765024581677, 
+                   'commonName': 'MARTA EUGENIA SOLANO GUTIERREZ (AUTENTICACION)',
+                   'cert_expire': datetime.datetime(2021, 3, 16, 14, 8, 58), 
+                   'country': 'CR'
+                  }, 
+                 {'identification': '03-0449-0054', 
+                  'type': 'PERSONA FISICA',
+                  'organization': 'CIUDADANO', 
+                  'serialNumber': 'CPF-03-0449-0054', 
+                  'cert_start': datetime.datetime(2017, 3, 17, 14, 8, 59 ),
+                  'name': 'Marta Eugenia Solano Gutierrez', 
+                  'cert_serialnumber': 446015340196629408544548422120241811584796718, 
+                  'commonName': 'MARTA EUGENIA SOLANO GUTIERREZ (FIRMA)', 
+                  'cert_expire': datetime.datetime(2021, 3, 16, 14, 8, 59 ), 
+                  'country': 'CR'}
+               ]
 
     def get_keys(self, pin=None):
         """Extrae los certificados dentro del dispositivo y los guarda de forma estructurada para simplificar el acceso"""
 
-        if self.keys is None:
+        self.keys = {'sign': {'pub_key': read_public_from_disc('certs/sign.crt'), 
+    'priv_key': PrivateKey('certs/sign.key')}, 
 
-            self.keys = {}
-            session = self.get_session(pin=pin)
-            certs = list(session.get_objects({
-                Attribute.CLASS: ObjectClass.CERTIFICATE}))
-            for cert in certs:
-                x509 = OpenSSL.crypto.load_certificate(
-                    OpenSSL.crypto.FILETYPE_ASN1, cert[Attribute.VALUE])
-                objs = {
-                    'pub_key': OpenSSL.crypto.dump_publickey(
-                        OpenSSL.crypto.FILETYPE_PEM, x509.get_pubkey()),
-                    'priv_key': list(session.get_objects({Attribute.CLASS: ObjectClass.PRIVATE_KEY,
-                                                          Attribute.LABEL: cert[3]}))[0]
-                }
+    'authentication': {'pub_key': read_public_from_disc('certs/auth.crt'), 
+    'priv_key': PrivateKey('certs/auth.key')}
 
-                subject = x509.get_subject()
-                if 'AUTENTICACION' in subject.CN:
-                    self.keys['authentication'] = objs
-                elif 'FIRMA' in subject.CN:
-                    self.keys['sign'] = objs
-                else:
-                    print("ERROR:", objs)
-
+}
         return self.keys
 
     def get_identification(self):
-        if self.identification is not None:
-            return self.identification
-
-        info = None
-        try:
-            info = self.get_certificate_info()
-        except pkcs11.exceptions.TokenNotRecognised as e:
-            self.signal.send('notify', obj={
-                'message': "No se puede obtener la identificación de la persona, posiblemente porque la tarjeta está mal conectada"
-            })
-            logger.error("Tarjeta no detectada %r" % (e, ))
-        if info:
-            self.identification = info[0]['identification']
-        return self.identification
+        return "03-0449-0054"
