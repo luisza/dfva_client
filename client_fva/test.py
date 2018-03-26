ef796b2b9a75aab6f251bfdcfa3ded2abb6def93
Luis Zárate M <luis.zarate@solvosoft.com>
2018-03-26 17:50:01 -0600

    test pdf

--- client_fva/test.py
+++ client_fva/test.py
@@ -14,5 +14,5 @@ client.authenticate('04-0212-0119', wait=True)
 client.sign('08-0888-0888',  None, "Readme file test", file_path='README.md', wait=True)
 
 client.validate(None, file_path='README.md')
-client.validate(None, file_path='README.md', _format='cofirma')
+client.validate(None, file_path='README.md', _format='pdf')
 client.is_suscriptor_connected('08-0888-0888')
