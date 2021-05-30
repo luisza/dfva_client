d6df1e24e8c1a5f4e914cc00ae5f2587f7f0c4d0
Luis Zárate M <luis.zarate@solvosoft.com>
2021-05-30 17:13:08 -0600

    fixed linux installer

--- builder/ci/linux.sh
+++ builder/ci/linux.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 
 OLD_PATH=$(pwd)
+cd src
 
-mkdir -p /data/
-cp -a src /client_fva
-cd /client_fva/
+sed -i 's/http:\/\/localhost:8000/https:\/\/firmadigital.solvosoft.com/g' client_fva/user_settings.py
+sed -i 's/self.installation_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))/self.installation_path = "\/usr\/share\/client_fva\/"/g'  client_fva/user_settings.py
 
-cp /builder/debian/copyright /tmp/
-cp /builder/debian/postinst /tmp/
+pyinstaller --clean --onefile -n client_fva -i client_fva/ui/ui_elements/images/icon.ico --upx-dir=/usr/local/share/  --noconfirm --log-level=WARN --windowed --hidden-import 'pkcs11.defaults' main.py
 
-sed -i 's/http:\/\/localhost:8000/https:\/\/firmadigital.solvosoft.com/g' /client_fva/client_fva/user_settings.py
-sed -i 's/self.installation_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))/self.installation_path = "\/usr\/share\/client_fva\/"/g'  /client_fva/client_fva/user_settings.py
-
-pyinstaller --clean --onefile -n client_fva -i client_fva/ui/ui_elements/images/icon.ico --upx-dir=/usr/local/share/  --noconfirm --log-level=WARN --windowed --distpath=/data/ --workpath=/tmp/client_fvabuild --hidden-import 'pkcs11.defaults' main.py
-
-mkdir -p /data/build/
-cd  /data/build/
+cd dist
 
 DEB_HOMEDIR="${PACKAGE}_${VERSION}_${ARCH}"
 mkdir -p $DEB_HOMEDIR/usr/share/client_fva/os_libs/linux/${ARCH}/
@@ -51,27 +44,26 @@ Categories=Network;Application;
 StartupNotify=true
 END
 
-cp /tmp/copyright $DEB_HOMEDIR/DEBIAN/
-cp /tmp/postinst $DEB_HOMEDIR/DEBIAN/
+cp $OLD_PATH/builder/debian/copyright $DEB_HOMEDIR/DEBIAN/
+cp $OLD_PATH/builder/debian/postinst $DEB_HOMEDIR/DEBIAN/
 sed -i 's/ARCH/'${ARCH}'/g' $DEB_HOMEDIR/DEBIAN/postinst
 chmod 0775 $DEB_HOMEDIR/DEBIAN/postinst
 
-cp /data/client_fva $DEB_HOMEDIR/usr/share/client_fva/client_fva.bin
-cp /client_fva/client_fva/ui/ui_elements/images/* $DEB_HOMEDIR/usr/share/client_fva/client_fva/ui/ui_elements/images/
-cp /client_fva/os_libs/Athena/IDPClientDB.xml $DEB_HOMEDIR/etc/Athena/client_fva_IDPClientDB.xml
-cp /client_fva/os_libs/linux/${ARCH}/libASEP11.so $DEB_HOMEDIR/usr/share/client_fva/os_libs/linux/${ARCH}/libASEP11.so
-cp -a /client_fva/certs/ $DEB_HOMEDIR/usr/share/client_fva/
+cp client_fva $DEB_HOMEDIR/usr/share/client_fva/client_fva.bin
+cp $OLD_PATH/src/client_fva/ui/ui_elements/images/* $DEB_HOMEDIR/usr/share/client_fva/client_fva/ui/ui_elements/images/
+cp $OLD_PATH/src/os_libs/Athena/IDPClientDB.xml $DEB_HOMEDIR/etc/Athena/client_fva_IDPClientDB.xml
+cp $OLD_PATH/src/os_libs/linux/${ARCH}/libASEP11.so $DEB_HOMEDIR/usr/share/client_fva/os_libs/linux/${ARCH}/libASEP11.so
+cp -a $OLD_PATH/src/certs/ $DEB_HOMEDIR/usr/share/client_fva/
 
 dpkg-deb --build --root-owner-group $DEB_HOMEDIR
 alien -r $DEB_HOMEDIR.deb --scripts
 alien -l $DEB_HOMEDIR.deb --scripts
 alien -t $DEB_HOMEDIR.deb --scripts
+ 
 
-cp /client_fva/  
-
-mv /data/build/${PACKAGE}_${VERSION}_${ARCH}.deb $OLD_PATH/
-mv /data/build/${PACKAGE}-${VERSION}-2.${RPM_ARCH}.rpm  $OLD_PATH/
-mv /data/build/lsb-${PACKAGE}-${VERSION}-2.${RPM_ARCH}.rpm $OLD_PATH/
-mv /data/build/${PACKAGE}-${VERSION}.tgz $OLD_PATH/
+mv build/${PACKAGE}_${VERSION}_${ARCH}.deb $OLD_PATH/
+mv build/${PACKAGE}-${VERSION}-2.${RPM_ARCH}.rpm  $OLD_PATH/
+mv build/lsb-${PACKAGE}-${VERSION}-2.${RPM_ARCH}.rpm $OLD_PATH/
+mv build/${PACKAGE}-${VERSION}.tgz $OLD_PATH/
  
 cd $OLD_PATH
