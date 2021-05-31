fcbde48a18c71b22ea68ba5bd9431006c33f89cc
Luis Zárate M <luis.zarate@solvosoft.com>
2021-05-31 16:58:27 -0600

    iscc builder

--- builder/ci/windows.sh
+++ builder/ci/windows.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-choco install innosetup
+choco install innosetup sqlite
 
 OLD_PATH=$(pwd)
 echo $OLD_PATH
@@ -8,16 +8,20 @@ echo $OLD_PATH
 cd src
 sed -i 's/http:\/\/localhost:8000/https:\/\/firmadigital.solvosoft.com/g' client_fva/user_settings.py
 
-pyinstaller --clean --onefile -n client_fva -i client_fva/ui/ui_elements/images/icon.ico --upx-dir=/usr/local/share/  --noconfirm --log-level=WARN --windowed --hidden-import 'pkcs11.defaults' main.py
+pyinstaller --clean --onefile -n client_fva -i client_fva/ui/ui_elements/images/icon.ico --upx-dir=/usr/local/share/  --noconfirm --log-level=WARN --windowed --noconsole --hidden-import 'pkcs11.defaults' main.py
 
 cd dist
 mkdir workdir
 EXE_HOMEDIR="workdir"
-mkdir -p $EXE_HOMEDIR/System32
-mkdir -p $EXE_HOMEDIR/Athena/
+mkdir -p $EXE_HOMEDIR/
 
-cp $OLD_PATH/src/os_libs/windows/asepkcs.dll $EXE_HOMEDIR/System32/
-cp $OLD_PATH/src/os_libs/Athena/IDPClientDB.xml $EXE_HOMEDIR/Athena/
+
+cp $OLD_PATH/src/os_libs/windows/asepkcs.dll $EXE_HOMEDIR/
 cp $OLD_PATH/src/dist/client_fva.exe $EXE_HOMEDIR/
+cp $OLD_PATH/src/client_fva/ui/ui_elements/images/icon.png $EXE_HOMEDIR/
+cp $OLD_PATH/src/os_libs/windows/installer.iss $EXE_HOMEDIR/
 
-ls -al $EXE_HOMEDIR/
+cd $EXE_HOMEDIR/
+iscc installer.iss
+
+ls -al .
